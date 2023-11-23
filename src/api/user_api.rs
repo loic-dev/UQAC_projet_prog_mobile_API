3d19fb21a5a318b9da6856f292b1538b859c9b46
loic-dev <loic.charrie.12@gmail.com>
2023-11-23 10:01:46 -0500

    Add type for ApiResponse ( success & failure )

--- src/api/user_api.rs
+++ src/api/user_api.rs
@@ -1,97 +1,68 @@
 use crate::{models::user_model::User, repository::mongodb_repo::MongoRepo};
-use mongodb::bson::oid::ObjectId;
 use rocket::{http::Status, Request, serde::json::Json, State};
 use crate::models::jwt_model::JWT;
-use crate::models::status_model::CustomStatus;
+use crate::models::status_model::{FailureResponse, SuccessResponse};
 
 #[get("/user/<path>")]
-pub fn get_user(db: &State<MongoRepo>, path: String, _key: JWT) -> Result<Json<User>, Status> {
+pub fn get_user(db: &State<MongoRepo>, path: String, _key: JWT) -> Result<Json<SuccessResponse<User>>, Json<FailureResponse>> {
     let id = path;
     if id.is_empty() {
-        return Err(Status::BadRequest);
+        return Err(Json::from(FailureResponse {
+            code: Status::BadRequest,
+            error: "Id is null".to_string()
+        }))
     };
     let user_detail = db.get_user(&id);
-    match user_detail {
-        Ok(user) => Ok(Json(user)),
-        Err(_) => Err(Status::InternalServerError),
-    }
-}
-
-#[put("/user/<path>", data = "<new_user>")]
-pub fn update_user(
-    db: &State<MongoRepo>,
-    path: String,
-    new_user: Json<User>,
-    _key: JWT
-) -> Result<Json<User>, Status> {
-    let id = path;
-    if id.is_empty() {
-        return Err(Status::BadRequest);
-    };
-    let data = User {
-        id: Some(ObjectId::parse_str(&id).unwrap()),
-        firstname: new_user.firstname.to_owned(),
-        lastname: new_user.lastname.to_owned(),
-        email: new_user.email.to_owned(),
-        password: new_user.password.to_owned(),
-    };
-    let update_result = db.update_user(&id, data);
-    match update_result {
-        Ok(update) => {
-            if update.matched_count == 1 {
-                let updated_user_info = db.get_user(&id);
-                return match updated_user_info {
-                    Ok(user) => Ok(Json(user)),
-                    Err(_) => Err(Status::InternalServerError),
-                };
-            } else {
-                return Err(Status::NotFound);
-            }
-        }
-        Err(_) => Err(Status::InternalServerError),
+    return match user_detail {
+        Ok(user) =>
+            Ok(Json::from(SuccessResponse {
+                code: Status::Accepted,
+                message: user
+            })),
+        Err(_) =>
+            Err(Json::from(FailureResponse {
+                code: Status::InternalServerError,
+                error: "Internal Server Error".to_string()
+            })),
     }
 }
 
 #[delete("/user/<path>")]
-pub fn delete_user(db: &State<MongoRepo>, path: String, _key: JWT) -> Result<Json<&str>, Status> {
+pub fn delete_user(db: &State<MongoRepo>, path: String, _key: JWT) -> Result<Json<SuccessResponse<String>>, Json<FailureResponse>> {
     let id = path;
     if id.is_empty() {
-        return Err(Status::BadRequest);
+        return Err(Json::from(FailureResponse {
+            code: Status::BadRequest,
+            error: "Id is null".to_string()
+        }))
     };
     let result = db.delete_user(&id);
-    match result {
-        Ok(res) => {
-            if res.deleted_count == 1 {
-                return Ok(Json("User successfully deleted"));
-            } else {
-                return Err(Status::NotFound);
-            }
-        }
-        Err(_) => Err(Status::InternalServerError),
+    return match result {
+        Ok(_) =>
+            Ok(Json::from(SuccessResponse {
+                code: Status::Accepted,
+                message: "User successfully deleted".to_string()
+            })),
+        Err(_) =>
+            Err(Json::from(FailureResponse {
+                code: Status::InternalServerError,
+                error: "Internal Server Error while deleting user".to_string()
+            })),
     }
 }
 
-/* #[get("/users")]
-pub fn get_all_users(db: &State<MongoRepo>) -> Result<Json<Vec<User>>, Status> {
-    let users = db.get_all_users();
-    match users {
-        Ok(users) => Ok(Json(users)),
-        Err(_) => Err(Status::InternalServerError),
-    }
-} */
-
 #[get("/api")]
-pub fn auth(_key: JWT) -> Result<Json<CustomStatus>, Json<CustomStatus>>{
-    Ok(Json::from(CustomStatus {
+pub fn auth(_key: JWT) -> Result<Json<SuccessResponse<String>>,()>{
+    Ok(Json::from(SuccessResponse {
         code: Status::Accepted,
         message: "JWT token is valid".to_string()
     }))
 }
 
 #[catch[401]]
-pub fn jwt_unauthorized(status: Status, _req: &Request) -> Result<Json<CustomStatus>, Json<CustomStatus>>{
-    Ok(Json::from(CustomStatus {
+pub fn jwt_unauthorized(status: Status, _req: &Request) -> Result<(),Json<FailureResponse>>{
+    Err(Json::from(FailureResponse {
         code: status,
-        message: "Unauthorized JWT".to_string()
+        error: "Unauthorized JWT".to_string()
     }))
 }
